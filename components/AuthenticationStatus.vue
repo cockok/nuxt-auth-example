12cec5ec20425d0d595ac0ef62a033a4a860ed01
cockok <cockok@cheki.net>
2023-04-19 19:48:14 +0900

    custom login

--- components/AuthenticationStatus.vue
+++ components/AuthenticationStatus.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 const { status, data, signOut, signIn } = useSession()
+const email = ref('')
+const password = ref('')
+const handleSubmit = async () => {
+  await signIn('credentials', { email: email.value, password: password.value })
+}
 </script>
 
 <template>
@@ -22,10 +27,16 @@ const { status, data, signOut, signIn } = useSession()
         <button v-if="status === 'authenticated'" class="flex items-center justify-center space-x-2 bg-red-500 text-white rounded-lg py-2 px-3 text-lg" @click="signOut({ callbackUrl: '/' })">
           <span>Logout</span>
         </button>
-        <button v-else class="flex items-center justify-center space-x-2 bg-green-500 text-white rounded-lg py-2 px-3 text-lg" @click="signIn()">
-          <i class="fa fa-right-to-bracket pt-0.5" />
-          <span>Login</span>
-      </button>
+        <form v-else class="flex items-center space-x-2" @submit.prevent="handleSubmit">
+          <label class="leading-6">Email:</label>
+          <input v-model="email" class="border p-1" type="email" placeholder="Email" />
+          <label>Password:</label>
+          <input v-model="password" class="border p-1" type="password" placeholder="Password" />
+          <button type="submit" class="flex items-center justify-center space-x-2 bg-green-500 text-white rounded-lg py-2 px-3 text-lg">
+            <i class="fa fa-right-to-bracket pt-0.5" />
+            <span>Login</span>
+          </button>
+        </form>
       </div>
     </div>
 </template>
